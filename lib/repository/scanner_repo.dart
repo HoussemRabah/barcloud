8c0f291c36f1e1711c064d2b1e771bba47fab4c9
FlutLab.io <>
2022-03-16 20:05:48 +0000

    Autosave

--- lib/repository/scanner_repo.dart
+++ lib/repository/scanner_repo.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
 
 class ScannerRepository {
   Widget scannerCamera({required Function onCapture}) {
-    return QRCodeDartScanView(
-        scanInvertedQRCode: true, onCapture: (result) => onCapture(result));
+    return SizedBox();
   }
 }
