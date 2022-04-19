b1fd1e8abbdac1dd1b298480f2afa780bb0eb666
houssem <houssemranah@gmail.com>
2022-04-19 12:54:46 +0100

    add font name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'UI/screens/pages/home.dart';
 import 'UI/screens/pages/login.dart';
 
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'barcloud',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
       home: LoginScreen(),
     );
