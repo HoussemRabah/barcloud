8dce77d9926d66eb337a0821bc8133fd5ca19f51
FlutLab.io <>
2022-04-27 02:24:26 +0000

    Autosave

--- lib/UI/screens/pages/taskScreen.dart
+++ lib/UI/screens/pages/taskScreen.dart
@@ -1,27 +1,24 @@
-import 'dart:math';
-
-import 'package:barcloud/UI/screens/pages/login.dart';
-import 'package:barcloud/UI/widgets/loading/loading.dart';
 import 'package:barcloud/UI/widgets/ui/titleline.dart';
-import 'package:barcloud/UI/widgets/users/userlabel.dart';
 import 'package:barcloud/core/constants.dart';
-import 'package:barcloud/modules/class.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../core/sys.dart';
 import '../../../logic/bloc/auth/auth_bloc.dart';
 import '../../../logic/bloc/task/task_bloc.dart';
+import '../../../modules/class.dart';
+import '../../widgets/cards/task.dart';
+import '../../widgets/headers/headerTasks.dart';
+import '../../widgets/loading/loading.dart';
+import 'login.dart';
 
-class TaskScreen extends StatefulWidget {
-  final Task task;
-  const TaskScreen({Key? key, required this.task}) : super(key: key);
+class TasksScreen extends StatefulWidget {
+  const TasksScreen({Key? key}) : super(key: key);
 
   @override
-  State<TaskScreen> createState() => _TaskScreenState();
+  State<TasksScreen> createState() => _TasksScreenState();
 }
 
-class _TaskScreenState extends State<TaskScreen> {
+class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -35,91 +32,59 @@ class _TaskScreenState extends State<TaskScreen> {
       ],
       child: SafeArea(
         child: Scaffold(
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: Container(
-            margin: EdgeInsets.all(8.0),
-            height: 60,
-            width: MediaQuery.of(context).size.width * 16.0,
-            decoration: BoxDecoration(borderRadius: radius, color: colorPrime),
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Démarrer la tache",
-                style: styleSimplePlus.copyWith(color: colorMain),
-              ),
-            ),
-          ),
           appBar: AppBar(
             backgroundColor: colorPrime,
             elevation: 0.0,
           ),
           backgroundColor: colorBack,
-          body: SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          body: Column(
             children: [
-              TitleLine(title: "de la part de"),
               BlocBuilder<TaskBloc, TaskState>(
                 builder: (context, state) {
-                  if (state is TaskStateLoaded)
-                    return FutureBuilder(
-                      future: widget.task.getCreator(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<TheUser?> snapshot) {
-                        if (snapshot.hasData) if (snapshot.data != null)
-                          return UserLabel(
-                              username: snapshot.data!.getFullName(),
-                              image: snapshot.data!.getImage(),
-                              sub: snapshot.data!.sub);
-                        return Loading();
-                      },
-                    );
-                  return Loading();
+                  if (state is TaskStateLoading) return Loading();
+                  return HeaderTasks(
+                    taskFinish: taskBloc.tasksFinished ?? 0,
+                    taskWaiting: taskBloc.tasksWaiting ?? 0,
+                  );
                 },
               ),
-              SizedBox(
-                height: 32.0,
-              ),
-              Container(
-                margin: EdgeInsets.all(8.0),
-                padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-                decoration:
-                    BoxDecoration(borderRadius: radius, color: colorMain),
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
-                      child: Text(
-                        widget.task.title,
-                        style: styleSimplePlus.copyWith(color: colorPrime),
+              BlocBuilder<TaskBloc, TaskState>(
+                builder: (context, state) {
+                  if (state is TaskStateLoading) return Loading();
+                  if (state is TaskStateError) return Loading();
+                  if (state is TaskStateEmpty) return Text("error");
+                  return Column(
+                    children: [
+                      TitleLine(title: "vos taches"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListView.builder(
+                          itemBuilder: (BuildContext context, int index) {
+                            return TaskCard(task: taskBloc.tasks!.where((element) => (element.process != TaskProcess.end)).toList()[index]);
+                          },
+                          itemCount: taskBloc.tasks!.where((element) => (element.process != TaskProcess.end)).length,
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
-                      child: Text(
-                        "deadline : ${timeToString(widget.task.deadline)}",
-                        style: styleSmall.copyWith(color: colorAccent),
+                      TitleLine(title: "tâches accomplies"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListView.builder(
+                          itemBuilder: (BuildContext context, int index) {
+                            return TaskCard(task: taskBloc.tasks![index]);
+                          },
+                          itemCount: taskBloc.tasks!.where((element) => (element.process == TaskProcess.end)).length,
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 32.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
-                      child: Text(
-                        widget.task.disc,
-                        style: styleSimple,
-                      ),
-                    ),
-                  ],
-                ),
+                    ],
+                  );
+                },
               ),
             ],
-          )),
+          ),
         ),
       ),
     );
