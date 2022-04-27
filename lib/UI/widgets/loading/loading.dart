dd65cac6a098b9b3283ef2095805c0ed6ec9ed08
houssem <houssemranah@gmail.com>
2022-04-27 01:30:35 +0100

    add loading

--- lib/UI/widgets/loading/loading.dart
+++ lib/UI/widgets/loading/loading.dart
@@ -10,6 +10,6 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
-    return Text("loading");
+    return Center(child: CircularProgressIndicator());
   }
 }
